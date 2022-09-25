3ef4ffc35c008daf0d35cd155c44e2b37c9956a1
Rodrigo - Xita rps <xitarps@gmail.com>
2022-09-25 19:57:55 -0300

    Add Posts scaffold and custom

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    message = 'Post criado com sucesso.'
+    return redirect_to post_url(@post), notice: message if @post.save
+
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description)
+  end
+end
