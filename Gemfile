2d02723898e7fd69109c3716d74d2ab8fb85d472
Rodrigo - Xita rps <xitarps@gmail.com>
2022-09-26 00:53:27 -0300

    Add Devise - authentication User

--- Gemfile
+++ Gemfile
@@ -46,3 +46,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "rails-i18n", "~> 7.0"
+
+gem "devise", "~> 4.8"
+gem "devise-i18n", "~> 1.10"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
+
+  devise_for :users, only: %i[sessions registrations passwords]
+
   resources :posts, only: %i[index show new create]
 end
