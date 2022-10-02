e03a3b65111de150af83147fe94762de146fc627
Rodrigo - Xita rps <xitarps@gmail.com>
2022-10-02 03:52:49 -0300

    Add edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
 
     render 'posts/index'
   end
+
+  def edit; end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path, notice: 'Seu perfil foi atualizado com sucesso'
+    else
+      flash.now[:alert] = current_user.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
